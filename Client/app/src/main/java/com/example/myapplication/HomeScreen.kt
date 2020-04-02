f5a8469c3d675593f0b7f15f112a32a42d3f9a15
Eliodor Popov <eliodor.popov@outlook.com>
2020-04-03 00:07:58 +0300

    add signalR

---
+++ Client/app/src/main/java/com/example/myapplication/HomeScreen.kt
@@ -0,0 +1,85 @@
+package com.example.myapplication
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MotionEvent
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import com.microsoft.signalr.HubConnection
+import com.microsoft.signalr.HubConnectionBuilder
+import com.microsoft.signalr.HubConnectionState
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.Item
+import com.xwray.groupie.ViewHolder
+import kotlinx.android.synthetic.main.home_screen.*
+import kotlinx.android.synthetic.main.chat_message_incoming.view.*
+import java.lang.Exception
+
+class HomeScreen : AppCompatActivity() {
+
+    lateinit var hubConnection: HubConnection;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home_screen)
+
+        var adapter = GroupAdapter<ViewHolder>()
+        chat_view.adapter = adapter
+
+        hubConnection = HubConnectionBuilder.create("http://10.0.2.2:5000/chathub").build();
+        hubConnection.on("ReceiveNewMessage", {message ->
+            adapter.add(ChatIncomingItem(message));
+        }, String::class.java);
+
+        if (hubConnection.connectionState == HubConnectionState.DISCONNECTED)
+            hubConnection.start();
+
+        send_button.setOnClickListener{
+            val text = message_input_field.text.toString()
+            message_input_field.text.clear()
+            adapter.add(ChatOutgoingItem(text))
+
+            if (hubConnection.connectionState == HubConnectionState.CONNECTED)
+                hubConnection.send("MessageFromServer", text)
+            else {
+                Toast.makeText(
+                    applicationContext,
+                    "" + hubConnection.connectionState,
+                    Toast.LENGTH_SHORT
+                ).show();
+            }
+
+            chat_view.smoothScrollToPosition(adapter.itemCount - 1);
+        }
+
+    }
+
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        if (currentFocus != null) {
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
+        }
+        return super.dispatchTouchEvent(ev)
+    }
+}
+
+class ChatIncomingItem(val text: String): Item<ViewHolder>(){
+    override fun bind(viewHolder: ViewHolder, position: Int){
+        viewHolder.itemView.textView.text = text
+    }
+
+    override fun getLayout(): Int {
+        return R.layout.chat_message_incoming
+    }
+}
+
+class ChatOutgoingItem(val text: String): Item<ViewHolder>(){
+    override fun bind(viewHolder: ViewHolder, position: Int){
+        viewHolder.itemView.textView.text = text
+    }
+
+    override fun getLayout(): Int {
+        return R.layout.chat_message_outgoing
+    }
+}
---
+++ Server/Server/Hubs/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task MessageFromServer(string message)
+        {
+            Console.WriteLine("Message received: " + message);
+            await Clients.Others.SendAsync("ReceiveNewMessage", message);
+        }
+    }
+}
